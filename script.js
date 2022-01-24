af02f5a5dfe4e38061980f4d8fe8738cc2f97c00
Ogu <august.gros@gmail.com>
2022-01-24 13:43:53 +0100

    add slade bar v2

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Change la couleur du input range
 document.querySelector(".scroll").oninput = function() {
-    this.style.background = 'linear-gradient(to right, gray 0%, gray ' + this.value + '%, #EBECF0 ' + this.value + '%, #EBECF0 100%)'
-  };
+  this.style.background = 'linear-gradient(to right, gray 0%, gray ' + this.value + '%,#EBECF0 ' + this.value + '%, #EBECF0 100%)'
+};
